4f05d61107cfd64b6727a73b9646f594d13106c9
Maddie Jennings <madisonlynnjennings@gmail.com>
2021-12-04 22:08:26 -0500

    some refactoring

--- 2021/Day3.js
+++ 2021/Day3.js
@@ -1,69 +1,48 @@
 const helpers = require('./../helpers');
 const input = helpers.fetchInput('2021', 'Day3', '\n')
 
-const testInput = ['00100', '11110', '10110', '10111', '10101', '01111', '00111', '11100', '10000', '11001', '00010', '01010']
-
-const countBits = arr => {
-  let zerosCount = Array(arr[0].length).fill(0);
-  let onesCount = Array(arr[0].length).fill(0);
+const getMostCommonBit = (arr, idx) => {
+  let zeroCount = 0;
+  let oneCount = 0;
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      if (arr[i][j] === '0') {
-        zerosCount[j]++;
-      } else {
-        onesCount[j]++;
-      }
+    if (arr[i][idx] === '0') {
+      zeroCount++;
+    } else {
+      oneCount++;
     }
   }
-  return {zerosCount, onesCount};
+  return (zeroCount > oneCount) ? '0' : '1';
 }
 
-const part2 = (arr, type, idx, zerosCount, onesCount) => {
+const part2 = (arr, type, idx, mostBit) => {
   if (arr.length === 1) {
-    console.log(`${type} is: ${parseInt(arr[0], 2)}`);
     return parseInt(arr[0], 2);
   } else {
-    let mostCommon = (zerosCount[idx] > onesCount[idx]) ? '0' : '1';
-    if (type === 'O2 generator rating') {
-      // keep numbers that have most common in the given index
-      let newArr = [];
-      for (let i = 0; i < arr.length; i++) {
-        if (arr[i][idx] === mostCommon) {
-          newArr.push(arr[i]);
-        }
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (type === 'O2' && arr[i][idx] === mostBit) {
+        newArr.push(arr[i]);
+      } else if (type === 'CO2' && arr[i][idx] !== mostBit) {
+        newArr.push(arr[i])
       }
-      let newCounts = countBits(newArr);
-      return part2(newArr, type, idx + 1, newCounts.zerosCount, newCounts.onesCount);
-    } else {
-      // keep numbers that do not have most common in the given index
-      let newArr = [];
-      for (let i = 0; i < arr.length; i++) {
-        if (arr[i][idx] !== mostCommon) {
-          newArr.push(arr[i])
-        }
-      }
-      let newCounts = countBits(newArr)
-      return part2(newArr, type, idx + 1, newCounts.zerosCount, newCounts.onesCount);
     }
+    let newMost = getMostCommonBit(newArr, idx + 1);
+    return part2(newArr, type, idx + 1, newMost);
   }
 }
 
 const part1 = arr => {
-  let {zerosCount, onesCount} = countBits(arr);
-  let genRating = part2(arr, 'O2 generator rating', 0, zerosCount, onesCount);
-  let scrubRating = part2(arr, 'CO2 scrubber rating', 0, zerosCount, onesCount);
+  let mostBit = getMostCommonBit(arr, 0);
+  let genRating = part2(arr, 'O2', 0, mostBit);
+  let scrubRating = part2(arr, 'CO2', 0, mostBit);
   console.log('part 2 solution:', genRating * scrubRating);
 
   let gamma = '';
   let epsilon = '';
-  for (let i = 0; i < zerosCount.length; i++) {
-    if (zerosCount[i] > onesCount[i]) {
-      gamma = gamma.concat('', '0');
-      epsilon = epsilon.concat('', '1');
-    } else {
-      gamma = gamma.concat('', '1');
-      epsilon = epsilon.concat('', '0');
-    }
+  for (let i = 0; i < arr[0].length; i++) {
+    mostBit = getMostCommonBit(arr, i);
+    gamma = gamma.concat('', mostBit);
+    epsilon = epsilon.concat('', (mostBit === '0') ? '1' : '0');
   }
   console.log('part 1 solution:', parseInt(gamma, 2) * parseInt(epsilon, 2));
 }
