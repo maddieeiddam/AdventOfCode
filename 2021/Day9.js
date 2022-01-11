8153bfcdc68dbb04ca8b8c71624d776ac90ce8fc
Maddie Jennings <madisonlynnjennings@gmail.com>
2022-01-10 21:08:39 -0500

    finished Day 9 part 2

--- 2021/Day9.js
+++ 2021/Day9.js
@@ -14,7 +14,41 @@ const isLowPoint = (arr, i, j) => {
   return basin
 }
 
-const part1 = arr => {
+const getBasin = (arr, i, j, acc) => {
+  helpers.getAdjacent(arr, i, j).forEach(adj => {
+    let newPoints = []
+    if (arr[adj[0]][adj[1]] !== '9') {
+      let adjStr = adj[0] + '-' + adj[1]
+      if (!acc[adjStr]) {
+        newPoints.push([adj[0], adj[1]])
+        acc[adj[0] + '-' + adj[1]] = true
+      }
+    }
+    if (newPoints.length === 0) {
+      return acc
+    } else {
+      newPoints.forEach(pt => {
+        return getBasin(arr, pt[0], pt[1], acc)
+      })
+    }
+  })
+  return acc
+}
+
+const findLargestBasins = (arr, lows) => {
+  let basinSizes = []
+  lows.forEach(low => {
+    let basinObj = {[low]: true}
+    let [i, j] = low.split('-').map(Number)
+    let basin = getBasin(arr, i, j, basinObj, 1)
+
+    basinSizes.push(Object.keys(basin).length)
+  })
+  basinSizes.sort((a, b) => b - a)
+  console.log('part 2 solution:', basinSizes[0] * basinSizes[1] * basinSizes[2])
+}
+
+const getLowPoints = arr => {
   let riskLevel = 0;
   let lowPoints = [];
   for (let i = 0; i < arr.length; i++) {
@@ -26,7 +60,7 @@ const part1 = arr => {
     }
   }
   console.log('part 1 solution:', riskLevel);
-  return lowPoints;
+  findLargestBasins(arr, lowPoints)
 }
 
-part1(input)
+getLowPoints(input)
